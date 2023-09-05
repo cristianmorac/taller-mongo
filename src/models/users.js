9b3653d0b24622ebfd59ad2d67660782bd0a217f
tlian0206 <cristianfmorac@gmail.com>
2023-09-05 10:04:35 -0500

    guardar cambios

--- src/models/users.js
+++ src/models/users.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose')
 // Schema
 const usersSchema = new mongoose.Schema({
     name:String,
+    lastname: String,
+    address: String,
+    phone_number: String,
+    birthdate: String,
     password:String,
 })
 
